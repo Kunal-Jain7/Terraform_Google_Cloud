cc46612c7408c003c80c5b9bf66f74b7f22078ae
Kunal-Jain7 <kunal70223@gmail.com>
2024-08-01 09:59:40 +0530

    updating the code for launching comput instance as per DEV-135

--- terraform/main.tf
+++ terraform/main.tf
@@ -38,3 +38,25 @@ resource "google_compute_firewall" "http-rule" {
 
   source_tags = ["http"]
 }
+
+resource "google_compute_instance" "nginx-instance" {
+  name         = "nginx-instance"
+  machine_type = "e2-micro"
+  zone         = "us-central1-a"
+  tags         = [tolist(google_compute_firewall.ssh-rule.target_tags)[0], tolist(google_compute_firewall.ssh-rule.target_tags)[0]]
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-12"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.client-vpc.id
+    access_config {
+      // Ephemeral IP
+    }
+  }
+
+  metadata_startup_script = file("${path.module}/webserver-install.sh")
+}
